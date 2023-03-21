93d7c1fe7a7402267e124a1e93befaf5ab3bfbc6
Pawel Kowalski <kowalski.pawel.r@gmail.com>
2023-03-21 14:00:13 +0100

    small fixes

--- onrobot_rg2_driver/src/rg2_driver_node.py
+++ onrobot_rg2_driver/src/rg2_driver_node.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+from modbus_tk.modbus import ModbusError
+from threading import Lock
+
 import rospy
 
 from sensor_msgs.msg import JointState
@@ -9,6 +12,12 @@ from rg2_modbus import Rg2Modbus
 
 
 class Rg2DriverNode:
+    MIN_RG2_FORCE: float = 3.0  # [N]
+    MAX_RG2_FORCE: float = 40.0  # [N]
+    MIN_RG2_RANGE: float = 0.0  # [N]
+    MAX_RG2_RANGE: float = 100.0  # [N]
+    TIMEOUT: float = 10.0  # [s]
+
     def __init__(self) -> None:
         rospy.init_node("rg2_driver_node", anonymous=False)
 
@@ -16,6 +25,8 @@ class Rg2DriverNode:
         frame_id = rospy.get_param('~base_link_frame', 'rg2_base_link')
         joint_name = rospy.get_param('~gripper_joint_name', 'rg2_gripper_joint')
         self.service_call_timeout_sec = rospy.get_param('~service_call_timeout_sec', 10.0)
+        self._rg2_force_range = (3.0, 40.0)  # [N]
+        self._lock = Lock()
 
         self._rg2_modbus = Rg2Modbus(ur_robot_ip)
 
@@ -23,56 +34,81 @@ class Rg2DriverNode:
         self._joint_state_msg.header.frame_id = frame_id
         self._joint_state_msg.name = [joint_name]
 
+        rate = rospy.Rate(0.5)
+        while not self._rg2_modbus.watchdog.is_active():
+            rospy.loginfo_throttle(5.0, 'Waiting for URscript Modbus watchdog activation...')
+            rate.sleep()
+
         self._joint_publisher = rospy.Publisher("/joint_states", JointState, queue_size=1)
 
         self._set_gripp_state_service = rospy.Service(
             "rg2/set_gripper_width", GripperState, self._set_grip_state
         )
 
-        rospy.Timer(rospy.Duration(0.1), self._pub_joint_state_cb)
+        rospy.Timer(rospy.Duration(0.05), self._pub_joint_state_cb)
 
         rospy.loginfo(f'[{rospy.get_name()}] Node started')
 
     def _set_grip_state(self, req: GripperStateRequest) -> GripperStateResponse:
-        rospy.loginfo(
-            f'[{rospy.get_name()}] Sending gripp request:\n'
-            f'\t - width: {req.width}\n\t - force: {req.force}'
-        )
+        try:
+            with self._lock:
+                if not self.MIN_RG2_FORCE <= req.force <= self.MAX_RG2_FORCE:
+                    raise ValueError(
+                        f'Force value must be between {self.MIN_RG2_FORCE} and {self.MAX_RG2_FORCE} [N]'
+                    )
+                if not self.MIN_RG2_RANGE <= req.width <= self.MAX_RG2_RANGE:
+                    raise ValueError(
+                        f'Width value must be between {self.MIN_RG2_RANGE} and {self.MAX_RG2_RANGE} [mm]'
+                    )
 
+                rospy.loginfo(
+                    f'[{rospy.get_name()}] Sending gripp request:\n'
+                    f'\t - width: {req.width}\n\t - force: {req.force}'
+                )
+
+                self._rg2_modbus.send_gripp_req(req.width, req.force)
+                grip_detected = self._wait_for_grip_completion(self.TIMEOUT)
+
+                rospy.loginfo(f'[{rospy.get_name()}] Grip action succes!')
+
+                return GripperStateResponse(True, grip_detected, None)
+
+        except (ValueError, TimeoutError, ModbusError, ConnectionError) as e:
+            rospy.logwarn(f'[{rospy.get_name()}] Grip action failed: {str(e)}')
+            return GripperStateResponse(False, False, str(e))
+
+    def _wait_for_grip_completion(self, timeout: float) -> bool:
         start_time = rospy.Time.now()
-        self._rg2_modbus.send_gripp_req(req.width, req.force)
+        rate = rospy.Rate(20)
 
         while not self._rg2_modbus.querry_rg_busy_flag():
-            rospy.sleep(0.05)
-
-            if (rospy.Time.now() - start_time).secs > self.service_call_timeout_sec:
-                rospy.logwarn(f'[{rospy.get_name()}] Grip action failed: timeout') 
-                return GripperStateResponse(False, False, 'Grip action failed: timeout')
+            if (rospy.Time.now() - start_time).secs > timeout / 10:
+                # The preset position of the gripper is the same as 
+                # the actual position before the service was sent
+                return self._rg2_modbus.querry_grip_detected_flag()
+            rate.sleep()
 
         rospy.loginfo(f'[{rospy.get_name()}] Request send succesful.')
 
         while self._rg2_modbus.querry_rg_busy_flag():
-            rospy.sleep(0.1)
+            if (rospy.Time.now() - start_time).secs > timeout:
+                raise TimeoutError('Grip action failed: timeout')
+            rate.sleep()
 
-            if (rospy.Time.now() - start_time).secs > self.service_call_timeout_sec:
-                rospy.logwarn(f'[{rospy.get_name()}] Grip action failed: timeout')
-                return GripperStateResponse(False, False, 'Grip action failed: timeout')
-        
-        grip_detected = self._rg2_modbus.querry_grip_detected_flag()
-        success = True
-        
-        rospy.loginfo(f'[{rospy.get_name()}] Grip action succes!')
-
-        return GripperStateResponse(success, grip_detected, '')
+        return self._rg2_modbus.querry_grip_detected_flag()
 
     def _pub_joint_state_cb(self, *args) -> None:
-        self._joint_state_msg.header.stamp = rospy.Time.now()
+        try:
+            self._joint_state_msg.header.stamp = rospy.Time.now()
 
-        rg_width = self._rg2_modbus.querry_gripper_width()
-        joint_state = self._map(rg_width, 0.0, 101.0, -0.44, 1.0)
-        self._joint_state_msg.position = [joint_state]
+            rg_width = self._rg2_modbus.querry_gripper_width()
+            joint_state = self._map(rg_width, 0.0, 101.0, -0.44, 1.0)
+            self._joint_state_msg.position = [joint_state]
 
-        self._joint_publisher.publish(self._joint_state_msg)
+            self._joint_publisher.publish(self._joint_state_msg)
+
+        except ConnectionError as e:
+            rospy.logerr_throttle(20.0, str(e))
 
     @staticmethod
     def _map(x: float, in_from: float, in_to: float, out_from: float, out_to: float) -> float:
--- onrobot_rg2_driver/src/rg2_modbus.py
+++ onrobot_rg2_driver/src/rg2_modbus.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 import modbus_tk.defines as cst
 from modbus_tk import modbus_tcp
-from time import sleep
-from typing import Tuple
+from threading import Thread
+
+import rospy
 
 
 @dataclass
@@ -10,9 +11,42 @@ class ColisConsts:
     WIDTH_OUT = 16
     RG_BUSY_OUT = 17
     GRIP_DETECTED_OUT = 18
+    WATCHDOG_OUT = 19
     ACTION_TRIGGER_IN = 20
     WIDTH_IN = 128
     FORCE_IN = 129
+
+
+class Watchdog:
+    WATCHDOG_TIMEOUT = rospy.Duration(0.2)
+
+    def __init__(self, master: modbus_tcp.TcpMaster) -> None:
+        self._master = master
+        self._prev_state = False
+        self._last_state_change_time = None
+
+        self.watchdog_thread = Thread(target=self._update_watchdog_state, name='watchdog_thread')
+        self.watchdog_thread.start()
+
+        rospy.sleep(self.WATCHDOG_TIMEOUT)
+
+    def _update_watchdog_state(self):
+        rate = rospy.Rate(20)
+
+        while True:
+            state = bool(self._master.execute(1, cst.READ_COILS, ColisConsts.WATCHDOG_OUT, 1)[0])
+
+            if state != self._prev_state:
+                self._last_state_change_time = rospy.Time.now()
+
+            self._prev_state = state
+            rate.sleep()
+
+    def is_active(self) -> bool:
+        if self._last_state_change_time is None:
+            return False
+        
+        return rospy.Time.now() - self._last_state_change_time < self.WATCHDOG_TIMEOUT
 
 
 class Rg2Modbus:
@@ -20,7 +54,11 @@ class Rg2Modbus:
         self._master = modbus_tcp.TcpMaster(host=ur_robot_ip, port=502)
         self._master.set_timeout(5.0)
 
+        self.watchdog = Watchdog(self._master)    
+
     def send_gripp_req(self, width: float, force: float) -> None:
+        self._check_modbus_connection()
+
         # set gripper width and force
         self._master.execute(
             1,
@@ -33,16 +71,26 @@ class Rg2Modbus:
         self._master.execute(
             1, cst.WRITE_SINGLE_COIL, ColisConsts.ACTION_TRIGGER_IN, output_value=1
         )
-        sleep(0.1)
+        rospy.sleep(0.05)
         self._master.execute(
             1, cst.WRITE_SINGLE_COIL, ColisConsts.ACTION_TRIGGER_IN, output_value=0
         )
 
     def querry_rg_busy_flag(self) -> bool:
+        self._check_modbus_connection()
         return self._master.execute(1, cst.READ_COILS, ColisConsts.RG_BUSY_OUT, 1)[0]
 
     def querry_grip_detected_flag(self) -> bool:
+        self._check_modbus_connection()
         return self._master.execute(1, cst.READ_COILS, ColisConsts.GRIP_DETECTED_OUT, 1)[0]
 
-    def querry_gripper_width(self):
+    def querry_gripper_width(self) -> float:
+        self._check_modbus_connection()
         return self._master.execute(1, cst.READ_HOLDING_REGISTERS, ColisConsts.WIDTH_OUT, 1)[0] / 10
+    
+    def _check_modbus_connection(self):
+        if not self.watchdog.is_active():
+            raise ConnectionError(
+                'The URscript has stopped running (watchdog is inactive).'
+                'To restart it, use the VNC server or the Universal Robots ROS driver Dashboard.'
+            )
